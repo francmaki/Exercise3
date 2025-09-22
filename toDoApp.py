af21c6effa206ef5d0dd14696a7d97c36678a8d0
Aldrindave09 <aldrinbalagao@gmail.com>
2025-09-22 17:59:48 +0800

    Input Validation Fixed

--- toDoApp.py
+++ toDoApp.py
@@ -1,39 +1,48 @@
 # toDoApp.py
 
-tasks=[]
-
-def addtask(task) :
-  tasks.append(task)
-  print("task added!")
-
-def showTasks( ):
-    if len(tasks)==0 :
-      print("no tasks yet")
+tasks = []
+def addtask(task):
+    if task.strip() == "":
+        print("Task cannot be empty!")
     else:
-     for i in range (len(tasks)):
-      print(i+1,".",tasks[i])
-
+        tasks.append(task)
+        print("Task added!")
+def showTasks():
+    if len(tasks) == 0:
+        print("No tasks yet")
+    else:
+        for i in range(len(tasks)):
+            print(i + 1, ".", tasks[i])
 def removetask(tasknumber):
-    tasks.pop(tasknumber) 
-    print("task removed!!")
-
+    if tasknumber < 1 or tasknumber > len(tasks):
+        print("Invalid task number!")
+    else:
+        tasks.pop(tasknumber - 1)
+        print("Task removed!")
 def main():
     while True:
-        print("1 Add Task")
         print("2.Show Tasks")
+        print("1 Add Task")
         print("3.Remove Task")
         print("4- Exit")
-        ch = input("enter choice : ")
-        if ch=="1":
-            t = input("enter task : ")
+        ch = input("Enter choice: ")
+        if ch == "1":
+            t = input("Enter task: ")
             addtask(t)
-        elif ch=="2":
+        elif ch == "2":
             showTasks()
-        elif ch=="3":
-            n=int(input("enter task no to remove: "))
-            removetask(n)   
-        elif ch=="4":
-            break;
+        elif ch == "3":
+            if len(tasks) == 0:
+                print("No tasks to remove!")
+                continue
+            try:
+                n = int(input("Enter task no to remove: "))
+                removetask(n)
+            except ValueError:
+                print("Invalid input! Enter a number.")
+        elif ch == "4":
+            break
         else:
-            print("wrong choice!!")
+            print("Wrong choice!!")
+
 main()
